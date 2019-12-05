3d42e98a16f0a3c640199c52c9bc4e8dda515a5e
LHunn <leyla.hunn@outlook.com>
2019-12-05 13:58:21 +0000

    day 1 part 2 solved

--- src/main/java/dayOne.java
+++ src/main/java/dayOne.java
@@ -10,32 +10,23 @@ Get the mass (input), divide by 3, round down, and subtract 2 for the fuel amoun
 public class dayOne {
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
-        float[] numbers = new float[3];
+        float[] numbers = new float[100];
         float result = 0;
         float total = 0;
-        int i;
-        double fuel=0;
-        double sum=0;
-        double refuel=0;
-
         float values = 0;
-        for (i = 0; i < numbers.length; i++) {
+
+        for (int i = 0; i < numbers.length; i++) {
             values = input.nextFloat();
-            result = (float) Math.floor(values /3) - 2;
-            values += result;
+            result = (float) Math.floor(values / 3) - 2;
             total += result;
-            for (int j = 0; j < values; j++) {
-                if((fuel = Math.floor(result /3) - 2) >0){
-                    refuel = Math.floor(fuel /3) - 2;
-                }
+            while(result != 0) {
+                result = (float) Math.floor(result / 3) - 2;
+                if(result <= 0) result = 0;
+                total += result;
             }
-            System.out.printf("fuel: %f", refuel);
-            //each fuel is the result of each input of input (so the equation applied twice to the input)
         }
         System.out.printf("Total altogether: %f \n ", total);
-        //total altogether = the values inputted /3 - 2. (equation applied once to each input then totalled)
-
-        //sum = total + totalFuel;
-        //System.out.printf("SUM: %f", sum);
     }
 }
+
+//solved
